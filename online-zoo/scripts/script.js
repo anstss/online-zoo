25af0d7ffd6cbc5153fc5d87de7b91f6e756c85c
anstss <anastasiya31017@gmail.com>
2021-04-25 21:13:20 +0300

    feat: add working dropdown

--- online-zoo/scripts/script.js
+++ online-zoo/scripts/script.js
@@ -4,9 +4,23 @@ if (burgerMenu) {
     const iconBurgerMenu = document.querySelector(".burger-menu__icon");
     const nav = document.querySelector(".nav");
 
-    burgerMenu.addEventListener("click", function(e) {
+    burgerMenu.addEventListener("click", function(event) {
         nav.classList.toggle("_active");
         burgerMenu.classList.toggle("_active");
         document.body.classList.toggle("_lock");
     })
 }
+
+const activeSidebarAnimal = document.querySelector(".side-bar__animals-card_active");
+
+activeSidebarAnimal.addEventListener("click", function(event) {
+    event.preventDefault();
+});
+
+const sidebar = document.querySelector(".side-bar");
+const openSidebar = document.querySelector(".side-bar__open");
+
+openSidebar.addEventListener("click", function(event) {
+    sidebar.classList.toggle("side-bar__opened");
+    document.body.classList.toggle("_lock");
+});
